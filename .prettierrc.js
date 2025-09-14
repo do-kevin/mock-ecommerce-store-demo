8db96b1938247e087e67f375ebe6c4b406cf26de
Kevin Do <do.kevin.vo@gmail.com>
2025-09-14 10:41:48 -0700

    update: add workspace configs

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+// @ts-check
+/// <reference types="@shopify/prettier-plugin-liquid" />
+
+/**
+ * @type {import('prettier').Options}
+ */
+module.exports = {
+  plugins: ["@shopify/prettier-plugin-liquid"],
+};
